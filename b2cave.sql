445bb129dc377a7bb9cdf593add9d9b556030c13
atomgenie <atomgenie@me.com>
2017-09-26 00:33:06 +0200

    BDD

---
+++ b2cave.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : tamtangucxb2cave.mysql.db
+-- Généré le :  mar. 26 sep. 2017 à 00:30
+-- Version du serveur :  5.6.34-log
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `tamtangucxb2cave`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `devoirs`
+--
+
+CREATE TABLE `devoirs` (
+  `id` int(255) NOT NULL,
+  `curse` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `day` int(2) NOT NULL DEFAULT '0',
+  `month` int(2) NOT NULL DEFAULT '0',
+  `suggest` int(1) NOT NULL DEFAULT '0',
+  `public` int(1) NOT NULL DEFAULT '1',
+  `id_user` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `end_devoirs`
+--
+
+CREATE TABLE `end_devoirs` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `id_devoirs` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `general`
+--
+
+CREATE TABLE `general` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `infos`
+--
+
+CREATE TABLE `infos` (
+  `id` int(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notepad`
+--
+
+CREATE TABLE `notepad` (
+  `id` int(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `ref` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `QCM`
+--
+
+CREATE TABLE `QCM` (
+  `id` int(255) NOT NULL,
+  `curse` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `modo` tinyint(1) NOT NULL DEFAULT '0',
+  `ban` varchar(255) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `devoirs`
+--
+ALTER TABLE `devoirs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `end_devoirs`
+--
+ALTER TABLE `end_devoirs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `general`
+--
+ALTER TABLE `general`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `infos`
+--
+ALTER TABLE `infos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `notepad`
+--
+ALTER TABLE `notepad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `QCM`
+--
+ALTER TABLE `QCM`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `devoirs`
+--
+ALTER TABLE `devoirs`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `end_devoirs`
+--
+ALTER TABLE `end_devoirs`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `general`
+--
+ALTER TABLE `general`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `infos`
+--
+ALTER TABLE `infos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `notepad`
+--
+ALTER TABLE `notepad`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `QCM`
+--
+ALTER TABLE `QCM`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
